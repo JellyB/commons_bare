d5d0f40570d5d22b997c0adf2be19279dcd1c412
韩超 <yan_che@qq.com>
2017-08-29 21:06:58 +0800

    update

--- common-sdk/common-spring-web/src/main/java/com/huatu/common/spring/web/advice/WrapperResponseBodyAdvice.java
+++ common-sdk/common-spring-web/src/main/java/com/huatu/common/spring/web/advice/WrapperResponseBodyAdvice.java
@@ -4,6 +4,7 @@ import com.huatu.common.CommonErrors;
 import com.huatu.common.Result;
 import com.huatu.common.SuccessMessage;
 import com.huatu.common.SuccessResponse;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -11,14 +12,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
+ * 如果不是spring boot环境，应该会忽略掉conditional，未测试
  * Created by shaojieyue
  * Created time 2016-04-18 09:56
  */
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+@ConditionalOnProperty(value = "wrapper.enabled",havingValue = "true")
 public class WrapperResponseBodyAdvice implements ResponseBodyAdvice {
 
     @Override
