27ac19342d74831760bd9b20ad81623c32461ba3
韩超 <yan_che@qq.com>
2017-09-19 19:04:34 +0800

    重新错误处理器

---
+++ common-base/src/main/java/com/huatu/common/bean/BaseValue.java
@@ -0,0 +1,11 @@
+package com.huatu.common.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author hanchao
+ * @date 2017/9/19 13:48
+ */
+public class BaseValue implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
--- common-base/src/main/java/com/huatu/common/exception/UnauthorizedException.java
+++ common-base/src/main/java/com/huatu/common/exception/UnauthorizedException.java
@@ -8,11 +8,18 @@ import com.huatu.common.ErrorResult;
  * @date 2017/8/24 10:05
  */
 public class UnauthorizedException extends BizException {
+    public UnauthorizedException(){
+        super(CommonErrors.PERMISSION_DENIED);
+    }
     public UnauthorizedException(String customMessage) {
         super(CommonErrors.PERMISSION_DENIED,customMessage);
     }
 
     public UnauthorizedException(ErrorResult errorResult){
         super(errorResult);
+    }
+
+    public UnauthorizedException(ErrorResult errorResult,String customMessage) {
+        super(errorResult,customMessage);
     }
 }
