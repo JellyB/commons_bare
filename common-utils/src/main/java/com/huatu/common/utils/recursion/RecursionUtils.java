5ce3a211be894885553efc81d5774cca3e245907
韩超 <yan_che@qq.com>
2017-10-06 20:25:43 +0800

    update

--- common-utils/src/main/java/com/huatu/common/utils/recursion/RecursionUtils.java
+++ common-utils/src/main/java/com/huatu/common/utils/recursion/RecursionUtils.java
@@ -15,12 +15,27 @@ public class RecursionUtils {
      * @param <T>
      * @return
      */
-    public static <T> void recursion(T t,RecursionFunc<T> func){
+    public static <T> void collect(T t,CollectionFunc<T> func){
         Collection<T> recursion = func.recursion(t);
         if(CollectionUtils.isNotEmpty(recursion)){
             for (T tmp : recursion) {
-                recursion(tmp,func);
+                collect(tmp,func);
             }
+        }
+    }
+
+    /**
+     * 递归处理数据,返回最后节点
+     * @param t
+     * @param func
+     * @param <T>
+     */
+    public static <T> T deal(T t,ObjectFunc<T> func){
+        T recursion = func.recursion(t);
+        if(recursion != null){
+            return deal(recursion,func);
+        }else{
+            return t;
         }
     }
 
@@ -29,10 +44,16 @@ public class RecursionUtils {
      * 递归的出口是返回了空的集合
      * @param <T>
      */
-    public interface RecursionFunc<T>{
+    public interface CollectionFunc<T>{
         Collection<T> recursion(T t);
     }
 
-
+    /**
+     * 以对象为出口
+     * @param <T>
+     */
+    public interface ObjectFunc<T>{
+        T recursion(T t);
+    }
 
 }
