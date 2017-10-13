8e1ac41c080344b32ac825f022191a6ec0da368b
韩超 <yan_che@qq.com>
2017-10-13 17:42:22 +0800

    赏金任务dev

---
+++ common-base-tiku/src/main/java/com/huatu/tiku/common/bean/RewardMessage.java
@@ -0,0 +1,22 @@
+package com.huatu.tiku.common.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author hanchao
+ * @date 2017/10/12 20:15
+ */
+@Data
+public class RewardMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String bizId; //业务id
+    private String action; //动作
+    private int gold; //大于0则优先使用
+    private int experience; //经验,大于0则优先使用
+    private int uid;
+    private String uname;
+    private int timestamp;
+}
