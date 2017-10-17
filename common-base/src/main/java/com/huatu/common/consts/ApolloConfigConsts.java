86fa182b6c82de7bb201171ac154308abdc57811
韩超 <yan_che@qq.com>
2017-10-17 20:44:38 +0800

    配置监听

---
+++ common-base/src/main/java/com/huatu/common/consts/ApolloConfigConsts.java
@@ -0,0 +1,18 @@
+package com.huatu.common.consts;
+
+/**
+ * @author hanchao
+ * @date 2017/10/17 18:26
+ */
+public class ApolloConfigConsts {
+    public static final String NAMESPACE_DEFAULT = "";
+
+    public static final String NAMESPACE_TIKU_BASIC = "tiku.basic";
+
+    public static final String NAMESPACE_HT_METRICS = "htonline.metrics";
+
+    public static final String NAMESPACE_TIKU_RABBIT = "tiku.rabbitmq";
+
+    public static final String NAMESPACE_TIKU_REDIS = "tiku.redis-cluster";
+
+}
