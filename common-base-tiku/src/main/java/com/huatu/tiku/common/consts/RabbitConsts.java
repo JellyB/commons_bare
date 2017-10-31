10780bca9bcea69efee2c8d4959320fc4b8cbf65
韩超 <yan_che@qq.com>
2017-10-31 10:17:35 +0800

    增加死信队列配置

---
+++ common-base-tiku/src/main/java/com/huatu/tiku/common/consts/RabbitConsts.java
@@ -0,0 +1,26 @@
+package com.huatu.tiku.common.consts;
+
+/**
+ * @author hanchao
+ * @date 2017/10/31 10:16
+ */
+public class RabbitConsts {
+    public static final String DLX = "dead_letter_exchange";
+    public static final String DLQ = "dead_letter_queue_default";
+    public static final String DLK = "dead_letter_route_default";
+
+
+    /**
+     * 免费课赠送
+     */
+    @Deprecated
+    public static final String QUEUE_SEND_FREE_COURSE = "send_free_course_queue";
+    /**
+     * 用户昵称更新
+     */
+    public static final String QUEUE_USER_NICK_UPDATE = "user_nick_update_queue";
+    /**
+     * 每日任务
+     */
+    public static final String QUEUE_REWARD_ACTION = "reward_action_queue";
+}
