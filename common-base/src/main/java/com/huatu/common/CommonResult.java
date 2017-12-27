cfa19c975a8041b0dfbff12847ecb29997db6fc7
韩超 <yan_che@qq.com>
2017-12-27 17:42:51 +0800

    add result

--- common-base/src/main/java/com/huatu/common/CommonResult.java
+++ common-base/src/main/java/com/huatu/common/CommonResult.java
@@ -8,11 +8,13 @@ package com.huatu.common;
 public class CommonResult {
     public static final Result SUCCESS = SuccessMessage.create();
 
+
     public static final ErrorResult INVALID_ARGUMENTS = ErrorResult.create(1000101,"非法的参数");
     public static final  ErrorResult SERVICE_INTERNAL_ERROR = ErrorResult.create(1000102,"服务内部错误");
     public static final  ErrorResult RESOURCE_NOT_FOUND = ErrorResult.create(1000103,"资源未发现");
     public static final  ErrorResult PERMISSION_DENIED = ErrorResult.create(1000104,"权限拒绝");
     public static final  ErrorResult FORBIDDEN = ErrorResult.create(1000105,"非法请求");
+    public static final ErrorResult UNAUTHORIZED = ErrorResult.create(1000106,"权限不足");
 
     /**
      * 用户会话过期
