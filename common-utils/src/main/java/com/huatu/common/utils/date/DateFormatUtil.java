7d05cde63db3bbcc04c5cf29e706da6c76579d51
韩超 <yan_che@qq.com>
2018-01-24 14:56:35 +0800

    增加dateformat

--- common-utils/src/main/java/com/huatu/common/utils/date/DateFormatUtil.java
+++ common-utils/src/main/java/com/huatu/common/utils/date/DateFormatUtil.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.time.FastDateFormat;
 public class DateFormatUtil {
     public static final FastDateFormat DEFAULT_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
 
+    public static final FastDateFormat NORMAL_MONTH_FORMAT = FastDateFormat.getInstance("yyyy-MM");
     public static final FastDateFormat NORMAL_DAY_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd");
     public static final FastDateFormat NORMAL_TIME_FORMAT = FastDateFormat.getInstance("HH:mm:ss");
 
