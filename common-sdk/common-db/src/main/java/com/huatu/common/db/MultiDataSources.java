fff3cc58f3fa6bdeba3d6cb00552e1c95b815415
韩超 <yan_che@qq.com>
2018-04-20 14:41:09 +0800

    增加支持多数据源的配置

--- common-sdk/common-db/src/main/java/com/huatu/common/db/MultiDataSources.java
+++ common-sdk/common-db/src/main/java/com/huatu/common/db/MultiDataSources.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 /**
  * 为了省事，不实现datasource接口，算法等留待项目扩展或者别的模块实现
+ * 主动分离方案可以直接使用第三方的，也可以自己基于transactional实现(要考虑到spring事务传播的问题)
  * @author hanchao
  * @date 2018/4/20 14:11
  */
