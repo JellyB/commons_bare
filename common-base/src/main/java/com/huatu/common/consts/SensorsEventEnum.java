4131eefdd0feaad445e3224f55b1495e0fd306d8
zhangchong <zhangchong01@huatu.com>
2019-01-11 22:35:30 +0800

    add sensors events

--- common-base/src/main/java/com/huatu/common/consts/SensorsEventEnum.java
+++ common-base/src/main/java/com/huatu/common/consts/SensorsEventEnum.java
@@ -12,7 +12,19 @@ public enum SensorsEventEnum {
 	MOKAO_SURESIGNUP("HuaTuOnline_app_pc_HuaTuOnline_SimulatedSureSignUp", "模考大赛确认报名"),
 	MOKAO_STARTANSWER("HuaTuOnline_app_pc_HuaTuOnline_SimulatedStartAnswer", "模考大赛开始答题"),
 	MOKAO_ENDANSWER("HuaTuOnline_app_pc_HuaTuOnline_SimulatedEndAnswer", "模考大赛结束答题"),
-	SIGNUP_SUCCEED("HuaTuOnline_pc_HuaTuOnline_SignUpSucceed", "注册成功");
+	SIGNUP_SUCCEED("HuaTuOnline_pc_HuaTuOnline_SignUpSucceed", "注册成功"),
+	LOGIN_NAME("HuaTuOnline_user_name", "华图在线_登录用户名"),
+	REGISTER_TIME("HuaTuOnline_signup_time", "华图在线_注册时间"),
+	/**
+	 * 报考城市  (北京)
+	 */
+	EXAM_AREA("HuaTuOnline_examination_city", "华图在线_报考城市"),
+	/**
+	 * 考试类型(公务员北京)
+	 */
+	EXAM_CATEGORY_AREA("HuaTuOnline_examination_type", "华图在线_考试类型");
+	 
+	
 
 	private String code;
 	private String desc;
